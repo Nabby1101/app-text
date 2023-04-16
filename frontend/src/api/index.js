aa9b19d62b8aa7aa19783307b6cb00a7085eead4
Nabby1101 <nguyendienanhviet@gmail.com>
2023-04-16 22:30:40 +0700

    Nabby said: Added

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const api = Axios.create({
-    baseURL: 'https://nabby-app.onrender.com/',
+    baseURL: 'https://nabby-app.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
